b760b672b6a20e482a0ee0b3af57d813c6fb002b
Devi Vara Prasad <devi.prasad@genesys.com>
2019-11-19 20:26:23 +0530

    adding post to the method

--- server.js
+++ server.js
@@ -203,8 +203,8 @@ var userData = [{
     phoneNumber:"16503971085"
 }]
 
-app.get('/iot/userdata', async (req, res) => {
-   var device_id = req.query.device_id;
+app.post('/iot/userdata', async (req, res) => {
+   var device_id = req.body.device_id;
    if(!device_id){
        res.status(400).send({error:"no device_id is given"});
    }
